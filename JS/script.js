798e1b11a18043c84c62531d9a066a6aab5fb6f0
Max <maxsoltys47@gmail.com>
2022-10-23 22:21:51 +0300

    Completed task

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,14 @@
 $('.menu-btn').on('click', function(e){
      e.preventDefault();
+
+     let width = parseInt($('body').css('width'));
+     let ifVisible = $('body').css("overflow-y");
+     if(width <= 650 && ifVisible == 'visible'){
+          $('body').css("overflow-y", 'hidden');
+     }else{
+          $('body').css("overflow-y", 'visible');
+     }
+
      $(this).toggleClass('menu-btn_active');
      $('.menu').toggleClass('menu-active');
 });
@@ -10,10 +19,15 @@ $(".list").on('click', function(a){
      $('.menu').removeClass('menu-active');
 });
 
+
+$('.block_element2').toggle();
+
 $('.element').on('click', function(k){
      k.preventDefault();
-     $('.discription').slideToggle(1000);
-     $('.discription').animate({
+     $('.block_element2').slideToggle(1000);
+     $('.block_element2').animate({
           opasity: 0.5
-     }, 1000);
-})
+     }, 2000);
+     
+     /* $('.block_element1').css("height", "100%"); */
+});
